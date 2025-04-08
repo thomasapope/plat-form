cc5ea369b307ef974b20433de4bd8a76ce9712e3
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2025-04-08 13:05:41 -0500

    Add Leaderboard button and leaderboard autoload

--- scripts/Scores.gd
+++ scripts/Scores.gd
@@ -1,22 +1,24 @@
 extends VBoxContainer
 
-
 var label = preload("res://scenes/LeaderboardLabel.tscn")
-
-
 export var filepath = "res://victors.txt"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Load list of victors
-	var victors = ["Charles C.", "Landen R.", "Owen G."]
+	# Load list of victors from a text file
+	var file = File.new()
+	if file.file_exists(filepath):
+		file.open(filepath, File.READ)
+		var victors = []
+		while not file.eof_reached():
+			var line = file.get_line().strip_edges()
+			if line != "":
+				victors.append(line)
+		file.close()
 	
-	for i in range(victors.size()):
-		var new_label = label.instance()
-		new_label.text = victors[i]
-		add_child(new_label)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		for i in range(victors.size()):
+			var new_label = label.instance()
+			new_label.text = victors[i]
+			add_child(new_label)
+	else:
+		print("File not found: %s" % filepath)
